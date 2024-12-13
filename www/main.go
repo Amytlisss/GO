e7b3f7ffcec6258a17c0e09c3de74a9db2c26540
kmjnn <ZhavoronkovaNA444@yandex.ru>
2024-12-13 17:25:38 +0300

    changes
    изменения
    
    Co-Authored-By: Amytlisss <169712235+Amytlisss@users.noreply.github.com>
    Co-Authored-By: TitovaAn <169666907+TitovaAn@users.noreply.github.com>

--- www/main.go
+++ www/main.go
@@ -1,42 +1,97 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+// Инициализация базы данных
+func initDB() {
+	var err error
+	connStr := "user=postgres password=0000 dbname=priyutik sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Создание таблицы
+	sqlStmt := `
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    email TEXT
+);
+    `
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}
+
+// Закрытие базы данных
+func closeDB() {
+	db.Close()
+}
+
 type User struct {
-	Name                  string
-	Age                   uint16
-	Money                 int16
-	Avg_grades, happiness float64
-	Hobbies               []string
-}
-
-func (u *User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money : %d", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.Name = newName
+	Name  string
+	Phone string
+	Email string
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{"dfkjdols", 11, 0, 4.3, 0.8, []string{"football"}}
 	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, bob)
+	tmpl.Execute(w, nil)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "contacts page!")
 }
 
+func registerUser(u User) error {
+	_, err := db.Exec("INSERT INTO users (name, phone, email) VALUES ($1, $2, $3)", u.Name, u.Phone, u.Email)
+	return err
+}
+func register_page(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		tmpl, _ := template.ParseFiles("templates/register.html")
+		tmpl.Execute(w, nil)
+	} else if r.Method == http.MethodPost {
+		r.ParseForm()
+
+		user := User{
+			Name:  r.FormValue("name"),
+			Phone: r.FormValue("phone"),
+			Email: r.FormValue("email"),
+		}
+
+		if err := registerUser(user); err != nil {
+			http.Error(w, "Ошибка при сохранении данных", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "Регистрация успешна! Имя: %s, Телефон: %s, Электронная почта: %s", user.Name, user.Phone, user.Email)
+	}
+}
+
 func handleRequest() {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/register", register_page) // Убедитесь, что этот маршрут добавлен
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
+
 func main() {
+	initDB()
+	defer closeDB()
 	handleRequest()
 }
